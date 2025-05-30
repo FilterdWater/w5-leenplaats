6b523916f9776ce4a021991de3024adebac0680a
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-05-30 12:18:32 +0200

    Add darkmode & two ShadCN components
    Docs:
    - https://ui.shadcn.com/docs/dark-mode/vite
    - https://ui.shadcn.com/docs/components/button
    - https://ui.shadcn.com/docs/components/dropdown-menu

--- frontend/src/js/pages/App.tsx
+++ frontend/src/js/pages/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "@/js/components/theme-provider";
+import { ModeToggle } from "@/js/components/ui/mode-toggle";
+
 export default function App() {
   return (
     <>
-      <div className="bg-background text-foreground">Hello frontend!</div>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="p-4 flex flex-row gap-4 items-center">
+          <div className="bg-background text-foreground">Hello frontend!</div>
+          <ModeToggle />
+        </div>
+      </ThemeProvider>
     </>
   );
 }
