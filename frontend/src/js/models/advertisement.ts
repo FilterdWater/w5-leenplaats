ac0b3213df68fe996646255011ea37fb77a4b25c
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-06-25 14:19:53 +0200

    Use actual data instead of dummy data

--- frontend/src/js/models/advertisement.ts
+++ frontend/src/js/models/advertisement.ts
@@ -5,7 +5,7 @@ export interface Advertisement {
   title: string;
   description: string;
   price: number;
-  userId: number;
+  user_id: number;
   rentedBy?: number;
   rentedAt?: Date;
   rentedUntil?: Date;
@@ -23,22 +23,4 @@ export interface AdvertisementDTO {
   rentedBy?: number;
   rentedAt?: Date;
   rentedUntil?: Date;
-}
-
-export function advertisementToJSON(ad: Advertisement) {
-  return {
-    id: ad.id,
-    title: ad.title,
-    description: ad.description,
-    price: ad.price,
-    user_id: ad.userId,
-    rented_by: ad.rentedBy ?? null,
-    rented_at: ad.rentedAt ? ad.rentedAt.toISOString() : null,
-    rented_until: ad.rentedUntil ? ad.rentedUntil.toISOString() : null,
-    created_at: ad.createdAt.toISOString(),
-    updated_at: ad.updatedAt ? ad.updatedAt.toISOString() : null,
-    image_url: ad.imageUrl ?? null,
-    categories:
-      ad.categories?.map((cat) => ({ id: cat.id, name: cat.name })) ?? [],
-  };
 }
