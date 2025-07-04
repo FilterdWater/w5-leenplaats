4529066e0c95dc80f7cfe02f0db308cbb8e965b4
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-07-04 09:10:09 +0200

    profile_picture is now base64

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('address');
             $table->string('zip_code');
             $table->string('city');
-            $table->string('profile_picture')->nullable();
+            $table->longtext('profile_picture')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
